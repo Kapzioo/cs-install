6df35afb5594b527234beadb59bade7a9c3a28d8
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-12-06 10:19:52 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,7 @@
-apt install lib32gcc1 lib32stdc++6 libc6-i386 libcurl4-gnutls-dev:i386 libsdl2-2.0-0:i386
+sudo apt-get install lib32gcc1
+sudo useradd -m steam
+su - steam
+mkdir ~/Steam && cd ~/Steam
 wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-tar xvfz steamcmd_linux.tar.gz
+tar xf steamcmd_linux.tar.gz
 ./steamcmd.sh
