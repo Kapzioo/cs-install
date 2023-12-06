4b01ac8b4d2158301c42b8dd1294b0935d0f52fc
Kapzioo <71943086+Kapzioo@users.noreply.github.com>
2023-12-06 10:16:11 +0100

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,4 @@
+apt install lib32gcc1 lib32stdc++6 libc6-i386 libcurl4-gnutls-dev:i386 libsdl2-2.0-0:i386
+wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+tar xvfz steamcmd_linux.tar.gz
+./steamcmd.sh
